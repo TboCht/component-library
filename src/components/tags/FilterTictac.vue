5132919cd6f24f6a8a23670a719939d1398e6582
TboCht <thibaud_chatillon@msn.com>
2026-02-17 11:01:21 +0100

    fix: bg tictac

--- src/components/tags/FilterTictac.vue
+++ src/components/tags/FilterTictac.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="flex flex-none items-center gap-1 rounded-full border-2 px-[14px] py-2 label-xs bg-white my-1 border-base-900"
-    :class="active ? '  bg-base-900 text-white' : 'bg-white text-base-900'"
+    class="flex flex-none items-center gap-1 rounded-full border-2 px-[14px] py-2 label-xs my-1 border-base-900"
+    :class="active ? 'bg-base-900 text-white' : 'bg-white text-base-900'"
   >
     {{ title }}
   </div>
